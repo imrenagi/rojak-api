35a18e844d96747607283bb9e98975c1db57524d
Imre Nagi <imre.nagi2812@gmail.com>
2017-07-30 02:20:35 -0700

    Limit access/authroization of the endpoints used by crawler.

--- service-analytics/src/main/java/id/rojak/analytics/resource/NewsController.java
+++ service-analytics/src/main/java/id/rojak/analytics/resource/NewsController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -86,6 +87,7 @@ public class NewsController {
         ), HttpStatus.OK);
     }
 
+    @PreAuthorize("#oauth2.hasScope('spider')")
     @RequestMapping(path = "/medias/{media_id}/elections/{election_id}/news/{news_id}",
             method = RequestMethod.POST)
     public ResponseEntity<NewsDTO> insertSentimentFor(
@@ -114,6 +116,7 @@ public class NewsController {
 
     }
 
+    @PreAuthorize("#oauth2.hasScope('spider')")
     @RequestMapping(path = "/elections/{election_id}/news",
             method = RequestMethod.GET)
     public ResponseEntity<NewsCollectionDTO> getNewsWithDateRange(
